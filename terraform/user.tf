f7afc5b0c11fad9eb9f7565516d2def9656fa847
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-03-07 13:42:56 -0500

    Resolve merge conflicts

--- terraform/user.tf
+++ terraform/user.tf
@@ -8,7 +8,6 @@ module "user" {
     aws.images-ssm              = aws.images_ssm
   }
 
-<<<<<<< HEAD
   entity = "ansible-role-burp-suite-pro"
   # The TF module will error if we don't put at least one value here.
   # This build user does not need to access any SSM parameters, so we
@@ -22,11 +21,4 @@ resource "aws_iam_role_policy_attachment" "thirdpartybucketread" {
 
   policy_arn = module.bucket_access.policy.arn
   role       = module.user.role.name
-=======
-  entity = "skeleton-ansible-role-with-test-user"
-
-  # If necessary, provide a list of SSM parameters that the test user needs to
-  # be able to read
-  # ssm_parameters = ["/example/parameter"]
->>>>>>> 26819a903c52b39dce7b3673f54402d3d281fba3
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,7 @@
 
 variable "burp_suite_pro_bucket" {
   description = "The name of the S3 bucket where the Burp Suite Pro installer lives."
+  nullable    = false
   type        = string
 }
 
@@ -24,7 +25,7 @@ variable "terraform_state_bucket" {
 variable "aws_region" {
   default     = "us-east-1"
   description = "The AWS region to deploy into (e.g. us-east-1)."
-<<<<<<< HEAD
+  nullable    = false
   type        = string
 }
 
@@ -33,11 +34,8 @@ variable "burp_suite_pro_objects" {
     "burpsuite_pro_linux.sh",
   ]
   description = "The Burp Suite Pro installer object(s) inside the bucket."
-  type        = list(string)
-=======
   nullable    = false
-  type        = string
->>>>>>> 26819a903c52b39dce7b3673f54402d3d281fba3
+  type        = list(string)
 }
 
 variable "tags" {
@@ -46,9 +44,6 @@ variable "tags" {
     Application = "ansible-role-burp-suite-pro testing"
   }
   description = "Tags to apply to all AWS resources created"
-<<<<<<< HEAD
-=======
   nullable    = false
->>>>>>> 26819a903c52b39dce7b3673f54402d3d281fba3
   type        = map(string)
 }
